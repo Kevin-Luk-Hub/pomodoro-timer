96581277f71898306ee332dc12f4b6196c003572
Kevin Luk <khl7wh@virginia.edu>
2020-07-01 04:28:06 -0400

    Added icons for sounds

--- public/app.js
+++ public/app.js
@@ -1,20 +1,23 @@
 const startButton = document.querySelector('.btn.start');
-const pauseButton = document.querySelector('.btn.pause');
 const resetButton = document.querySelector('.btn.reset');
 
-var totalTime = 1000;
-
+var totalTime = 10;
+var breakTime = 10;
 var minutes;
 var seconds;
 var timer;
+var paused = false;
 
 startButton.addEventListener('click', () => {
     clearInterval(timer);
     timer = setInterval(startTimer, 1000);
     if ((startButton.innerHTML = 'Start')) {
+        paused = true;
         startButton.innerHTML = 'Pause';
         console.log(startButton.innerHTML);
-    } else {
+    }
+
+    if ((startButton.innerHTML = 'Pause')) {
         startButton.innerHTML = 'Start';
         console.log(startButton.innerHTML);
     }
@@ -28,21 +31,19 @@ function startTimer() {
     minutes = Math.floor(totalTime / 60);
     seconds = totalTime - minutes * 60;
 
-    if (totalTime <= 0) {
+    if (totalTime < 0) {
         clearInterval(timer);
-        totalTime = 10 * 60;
+        totalTime = 25 * 60;
+        breakTimer();
     }
 
-    if (seconds < 10) {
-        document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
-    } else if (minutes < 10) {
-        document.getElementById('time').innerHTML = `0${minutes} : ${seconds}`;
-    } else if (minutes < 10 && seconds < 10) {
-        document.getElementById('time').innerHTML = `0${minutes} : $0{seconds}`;
-    } else {
-        document.getElementById('time').innerHTML = `${minutes} : ${seconds}`;
-    }
+    displayTime(minutes, seconds);
+
     totalTime -= 1;
+}
+
+function pauseTimer() {
+    clearInterval(timer);
 }
 
 function resetTimer() {
@@ -51,4 +52,37 @@ function resetTimer() {
     minutes = Math.floor(totalTime / 60);
     seconds = totalTime - minutes * 60;
     document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
+}
+
+function breakTimer() {
+    timer = setInterval(breakTimer, 1000);
+
+    minutes = Math.floor(breakTime / 60);
+    seconds = breakTime - minutes * 60;
+
+    if (breakTime <= 0) {
+        clearInterval(timer);
+        totalTime = 10 * 60;
+        breakTimer();
+    }
+
+    displayTime();
+
+    breakTime -= 1;
+}
+
+function displayTime(minutes, seconds) {
+    if (seconds < 10) {
+        document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
+    } else if (minutes < 10) {
+        document.getElementById('time').innerHTML = `0${minutes} : ${seconds}`;
+    } else if (minutes < 10 && seconds < 10) {
+        document.getElementById('time').innerHTML = `0${minutes} : 0${seconds}`;
+    } else {
+        document.getElementById('time').innerHTML = `${minutes} : ${seconds}`;
+    }
+}
+
+function rainSound() {
+    var audio = new Audio('.');
 }
