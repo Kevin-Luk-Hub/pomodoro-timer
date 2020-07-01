705bd62be389dbc88ae2c68f3a594e8ddaead5b1
Kevin Luk <khl7wh@virginia.edu>
2020-07-01 05:50:30 -0400

    Completed start, pause, and reset button functionalities

--- public/app.js
+++ public/app.js
@@ -1,25 +1,31 @@
 const startButton = document.querySelector('.btn.start');
 const resetButton = document.querySelector('.btn.reset');
 
-var totalTime = 10;
+var totalTime = 25 * 60;
 var breakTime = 10;
 var minutes;
 var seconds;
 var timer;
-var paused = false;
+var intervals;
+var paused = true;
+
+// long braek after 4 intervals
+if (intervals == 4) {
+}
 
 startButton.addEventListener('click', () => {
     clearInterval(timer);
     timer = setInterval(startTimer, 1000);
-    if ((startButton.innerHTML = 'Start')) {
-        paused = true;
+    if (paused) {
+        paused = false;
         startButton.innerHTML = 'Pause';
         console.log(startButton.innerHTML);
-    }
-
-    if ((startButton.innerHTML = 'Pause')) {
+        startTimer();
+    } else {
+        paused = true;
         startButton.innerHTML = 'Start';
         console.log(startButton.innerHTML);
+        pauseTimer();
     }
 });
 
@@ -31,45 +37,50 @@ function startTimer() {
     minutes = Math.floor(totalTime / 60);
     seconds = totalTime - minutes * 60;
 
-    if (totalTime < 0) {
+    if (totalTime <= 0) {
         clearInterval(timer);
+        intervals++;
         totalTime = 25 * 60;
-        breakTimer();
     }
 
     displayTime(minutes, seconds);
 
-    totalTime -= 1;
+    totalTime--;
 }
 
 function pauseTimer() {
     clearInterval(timer);
+    displayTime(minutes, seconds);
 }
 
 function resetTimer() {
     clearInterval(timer);
+    paused = true;
+    startButton.innerHTML = 'Start';
     totalTime = 25 * 60;
     minutes = Math.floor(totalTime / 60);
     seconds = totalTime - minutes * 60;
     document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
 }
 
-function breakTimer() {
-    timer = setInterval(breakTimer, 1000);
+// function breakTimer() {
+//     timer = setInterval(breakTimer, 1000);
 
-    minutes = Math.floor(breakTime / 60);
-    seconds = breakTime - minutes * 60;
+//     minutes = Math.floor(breakTime / 60);
+//     seconds = breakTime - minutes * 60;
 
-    if (breakTime <= 0) {
-        clearInterval(timer);
-        totalTime = 10 * 60;
-        breakTimer();
-    }
+//     if (breakTime <= 0) {
+//         clearInterval(timer);
+//         totalTime = 10 * 60;
+//         timer = setInterval(startTimer, 1000);
+//     }
 
-    displayTime();
+//     console.log(minutes, seconds);
 
-    breakTime -= 1;
-}
+//     displayTime();
+
+//     breakTime -= 1;
+// }
 
 function displayTime(minutes, seconds) {
     if (seconds < 10) {
