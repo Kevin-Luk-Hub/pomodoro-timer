a56977479819ad5d3d4c6e864d078fa2663a91ec
Kevin Luk <khl7wh@virginia.edu>
2020-07-01 00:59:04 -0400

    Working countdown timer

--- public/app.js
+++ public/app.js
@@ -1,11 +1,54 @@
 const startButton = document.querySelector('.btn.start');
 const pauseButton = document.querySelector('.btn.pause');
+const resetButton = document.querySelector('.btn.reset');
+
+var totalTime = 1000;
+
+var minutes;
+var seconds;
+var timer;
 
 startButton.addEventListener('click', () => {
-    startTimer();
+    clearInterval(timer);
+    timer = setInterval(startTimer, 1000);
+    if ((startButton.innerHTML = 'Start')) {
+        startButton.innerHTML = 'Pause';
+        console.log(startButton.innerHTML);
+    } else {
+        startButton.innerHTML = 'Start';
+        console.log(startButton.innerHTML);
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    resetTimer();
 });
 
 function startTimer() {
-    var minutes = 25;
-    document.getElementById('time').innerHTML = minutes + ' : ' + '00';
+    minutes = Math.floor(totalTime / 60);
+    seconds = totalTime - minutes * 60;
+
+    if (totalTime <= 0) {
+        clearInterval(timer);
+        totalTime = 10 * 60;
+    }
+
+    if (seconds < 10) {
+        document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
+    } else if (minutes < 10) {
+        document.getElementById('time').innerHTML = `0${minutes} : ${seconds}`;
+    } else if (minutes < 10 && seconds < 10) {
+        document.getElementById('time').innerHTML = `0${minutes} : $0{seconds}`;
+    } else {
+        document.getElementById('time').innerHTML = `${minutes} : ${seconds}`;
+    }
+    totalTime -= 1;
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    totalTime = 25 * 60;
+    minutes = Math.floor(totalTime / 60);
+    seconds = totalTime - minutes * 60;
+    document.getElementById('time').innerHTML = `${minutes} : 0${seconds}`;
 }
